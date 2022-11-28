63f4e2823dff5f6a2aecdeb20aa9e01bb5c3ed38
Snowanda <andalasa1231@gmail.com>
2022-11-28 10:41:07 +0800

    Changed main's message

--- src/main/java/com/github/Snowanda/Main.java
+++ src/main/java/com/github/Snowanda/Main.java
@@ -2,6 +2,6 @@ package com.github.Snowanda;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Ho world!");
     }
 }
